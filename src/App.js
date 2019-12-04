bab2955ecb4245744a1950732d4809875d06c4c5
Amelia Scarbrough <scaram02@Amelias-MacBook-Air.local>
2019-12-04 08:22:38 +0100

    initialized project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>New project 3</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+// // this components folder maybe needs to go to a client folder? not sure why
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+           <p>Nav?</p> 
+        </div>
+    )
+}
+
+export default Navbar
