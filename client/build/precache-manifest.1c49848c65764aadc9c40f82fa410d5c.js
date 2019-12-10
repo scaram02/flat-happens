13c28b75a98e19c2e01ded73703c484ffd5010ce
Amelia Scarbrough <scaram02@Amelias-MacBook-Air.local>
2019-12-10 10:43:03 +0100

    build stuff?

---
+++ client/build/precache-manifest.1c49848c65764aadc9c40f82fa410d5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3030322605210c803c49f37d1667b45f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b07b35c340216bb0df7b",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c827caaeb63bb4310163",
+    "url": "/static/js/2.6beec21d.chunk.js"
+  },
+  {
+    "revision": "b07b35c340216bb0df7b",
+    "url": "/static/js/main.928e1865.chunk.js"
+  },
+  {
+    "revision": "1337678ae3301a2c8a7b",
+    "url": "/static/js/runtime-main.8eac158a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
