b2d59d1552e51892ba46494bb9fb44fb9c795b08
Amelia Scarbrough <scaram02@Amelias-MacBook-Air.local>
2019-12-10 12:24:47 +0100

    flat it,your way

--- client/build/precache-manifest.198368788aaae7aab95fd65c9df02574.js
+++ client/build/precache-manifest.198368788aaae7aab95fd65c9df02574.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "51fcd36f99b7a040b12dd4fdeb09a438",
+    "revision": "87be4c8fb46c1bb0fbd01ae7329f5847",
     "url": "/index.html"
   },
   {
@@ -8,19 +8,23 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.64a99b10.chunk.css"
   },
   {
-    "revision": "e440f346a7e6667c5026",
-    "url": "/static/css/main.e3a359f6.chunk.css"
+    "revision": "8a3e76acb7f7a21274f0",
+    "url": "/static/css/main.0fb6400f.chunk.css"
   },
   {
     "revision": "28f6ee365ee84ec2fd97",
     "url": "/static/js/2.2dab5f0d.chunk.js"
   },
   {
-    "revision": "e440f346a7e6667c5026",
-    "url": "/static/js/main.3ca3881a.chunk.js"
+    "revision": "8a3e76acb7f7a21274f0",
+    "url": "/static/js/main.ff3c2fce.chunk.js"
   },
   {
     "revision": "1337678ae3301a2c8a7b",
     "url": "/static/js/runtime-main.8eac158a.js"
+  },
+  {
+    "revision": "b6e870bcfd7fbe7c113036a73f520423",
+    "url": "/static/media/background.b6e870bc.jpg"
   }
 ]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48cfb8238990b732a0ee2e6705280c83.js"
+  "/precache-manifest.198368788aaae7aab95fd65c9df02574.js"
 );
 
 self.addEventListener('message', (event) => {
