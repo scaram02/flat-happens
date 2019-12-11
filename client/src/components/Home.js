5da4785c17fd2c29e282b6b6fde8615c15e5362e
daneklustig <danek.lustig@gmail.com>
2019-12-11 16:59:48 +0100

    styling fixed

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,8 +4,8 @@ import "./Home.css";
 export default class Home extends Component {
   render() {
     return (
-      <div className="home-container home-nav">
-        <div className="">
+      <div className="home-container ">
+        <div className="home-nav">
           <div className="header-container">
             <div className="teach-me-css-pls">
               <h1>FLAT happens!</h1>
